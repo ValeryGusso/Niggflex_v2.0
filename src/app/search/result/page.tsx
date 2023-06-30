081cb17eb9047f7777d7e841f311ce56f8359239
ValeryGusso <valery.gusso@yandex.ru>
2023-06-30 21:29:20 +0300

    fix deploy bug

--- src/app/search/result/page.tsx
+++ src/app/search/result/page.tsx
@@ -1,23 +1,16 @@
 import { FC } from 'react'
 import cls from './page.module.css'
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 
-const SearchResult: FC<InferGetServerSidePropsType<typeof getServerSideProps>> = async ({ searchParams }) => {
-	// console.log('RESULT')
-	// console.log('COMPONENT: ', new URLSearchParams(searchParams).toString())
+interface SearchProps {
+	params: {
+		id: number
+	}
+	searchParams: Record<string, string>
+}
+
+const SearchResult: FC<SearchProps> = async ({ searchParams }) => {
 	await new Promise(res => setTimeout(res, 2000))
 	return <div>SEARCH: {searchParams.name}</div>
 }
 
-export const getServerSideProps: GetServerSideProps = async context => {
-	// await new Promise(res => setTimeout(res, 2000))
-	const query = context.query
-	console.log('PROPS: ', context)
-	return { props: { query } }
-}
-
 export default SearchResult
-
-// export async function getServerSideProps<>(params:type) {
-
-// }
